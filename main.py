0dbaa1afab69936d06c62da26e688a96e1f58169
vinhluu011658 <luuquocvinh44412@gmail.com>
2026-03-17 16:13:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import requests
 import pandas as pd
+import gspread
+import json
+import os
+from oauth2client.service_account import ServiceAccountCredentials
 
+# ===== LOAD GOOGLE CREDS =====
+creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+client = gspread.authorize(creds)
+
+# ===== OPEN SHEET =====
+sheet = client.open("DATA_STOCK").sheet1
+
+# ===== SYMBOL LIST =====
 symbols = ["VCB", "BID", "CTG"]
 
 data_all = []
 
+# ===== CALL API =====
 for symbol in symbols:
     url = f"https://api-finance-t19.24hmoney.vn/v1/ios/stock/statistic-investor-history?symbol={symbol}"
     
@@ -22,7 +42,10 @@ for symbol in symbols:
     except Exception as e:
         print(f"Lỗi {symbol}: {e}")
 
+# ===== SAVE TO SHEET =====
 df = pd.DataFrame(data_all)
-df.to_csv("output.csv", index=False)
+
+sheet.clear()
+sheet.update([df.columns.values.tolist()] + df.values.tolist())
 
 print("DONE")
