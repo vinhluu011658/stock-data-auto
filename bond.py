a308363c3e527fdf83df580010f3bfe8fde33cd3
vinhluu011658 <luuquocvinh44412@gmail.com>
2026-04-18 19:29:00 +0700

    Update bond.py

--- bond.py
+++ bond.py
@@ -164,7 +164,7 @@ def update_sheet(sheet, df):
     # ⭐ ĐỔI . → , ở cột lãi suất
     df["Lãi suất phát hành (%/năm)"] = df["Lãi suất phát hành (%/năm)"].astype(str).str.replace(".", ",", regex=False)
     
-    sheet.batch_clear(["G1:Q11"])
+    sheet.batch_clear(["G1:Q101"])
     
     sheet.update(
         "G1",
