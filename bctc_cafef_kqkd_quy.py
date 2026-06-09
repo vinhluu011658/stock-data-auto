d5850fd154b4ed8619714b09e0c76bd3fa5a389b
vinhluu011658 <luuquocvinh44412@gmail.com>
2026-06-09 16:42:50 +0700

    Update bctc_cafef_kqkd_quy.py

--- bctc_cafef_kqkd_quy.py
+++ bctc_cafef_kqkd_quy.py
@@ -65,7 +65,7 @@ def get_kqkd(symbol):
         "https://apiweb.cafef.vn/api/v1/BCTC/GetReportDetail"
         f"?symbol={symbol}"
         "&pageIndex=1"
-        "&pageSize=4"
+        "&pageSize=10"
         "&reportType=KQKD"
         "&TypeTime=QUY"
     )
